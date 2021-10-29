75fc4c9fa7aa1c66cbed678020e0ccc3caeb0b34
Caliber <bradcale@yahoo.com>
2021-10-28 21:54:29 -0400

    first commit

---
+++ jabot.py
@@ -0,0 +1,59 @@
+# import depencies
+import tweepy 
+import schedule
+import random
+import time
+
+# enter API key credentials here
+consumer_key='XXXXXXXXXXXXXXXXXXXXXXXXXX'
+consumer_secret_key='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+access_token='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+access_token_secret='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+#authenticating to access the twitter API
+auth=tweepy.OAuthHandler(consumer_key,consumer_secret_key)
+auth.set_access_token(access_token,access_token_secret)
+api=tweepy.API(auth)
+
+# set up variables
+# number of quotes
+length_of_file = 6
+index_array = []
+
+for i in range(length_of_file):
+	index_array.append(0)	# 0 for unused quote
+### get a Jane Austin quote from a file
+def post_quote():
+	quote_file_name = "enter file name here (including file extension)"
+	# open file
+	qfile = open(quote_file_name, "r")
+
+	quote_index = random.randint(0,length_of_file-1)
+	while index_array[quote_index] == 1:
+		quote_index = random.randint(0,length_of_file-1)
+	quote = ""
+	for i in range(length_of_file-1):
+		quote = qfile.readline()
+	# post quote to twitter
+	api.update_status(quote)
+	# mark index as used
+	index_array[quote_index] = 1
+	# reset if all quotes used up
+	reset = False
+	for item in index_array:
+		if item == 1:
+			reset = True
+	if reset:
+		for item in index_array:
+			item = 0
+
+### set conditionals for posting quote
+schedule.every().monday.at("14:00").do(post_quote)
+schedule.every().tuesday.at("14:00").do(post_quote)
+schedule.every().wednesday.at("14:00").do(post_quote)
+schedule.every().thursday.at("14:00").do(post_quote)
+schedule.every().friday.at("14:00").do(post_quote)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
